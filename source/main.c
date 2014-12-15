8d46dcbc05035728203c825cb334890bd5b99bdd
astrofra <astrofra@gmail.com>
2014-12-15 08:13:01 +0100

    - Remplacement de tout le code d'init. écran par un exemple tiré du manuel "Amiga C".  - Ouvre 2 viewports de tailles et de profondeurs differentes (32 couleurs en haut, 8 couleurs en bas).

--- source/main.c
+++ source/main.c
@@ -1,223 +1,288 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <dos/dos.h>
-#include <exec/types.h>             /* The Amiga data types file.         */
-#include <exec/memory.h>             /* The Amiga data types file.         */
-#include <exec/libraries.h>
-#include <intuition/intuition.h>    /* Intuition data strucutres, etc.    */
-#include <libraries/dos.h>          /* Official return codes defined here */
-#include <devices/keyboard.h>
+/* Example 4                                                            */
+/* This example demonstrates how to open two different ViewPorts on the */
+/* same display. The first ViewPort is in low resolution and use 32     */
+/* colours, while the second ViewPort is in high resolution and only    */
+/* use 2 colours.                                                       */
 
-// #include <intuition/intuitionbase.h>
-#include <clib/exec_protos.h>       /* Exec function prototypes           */
-#include <clib/alib_protos.h>
-#include <clib/graphics_protos.h>       /* Exec function prototypes           */
-#include <clib/intuition_protos.h>  /* Intuition function prototypes      */
+#include "includes.prl"
+#include <intuition/intuition.h>
+#include <graphics/gfxbase.h>
 
 /*
-	Custom routines
+Custom routines
 */
 #include "bitmap_routines.h"
 
 /*
-	Graphic assets
+Graphic assets
 */
 #include "mandarine_logo.h"
 
-/* Use lowest non-obsolete version that supplies the functions needed. */
-#define INTUITION_REV 33L
 
-/* Keyboard device */
-struct MsgPort  *KeyMP;         /* Pointer for Message Port */
-struct IOStdReq *KeyIO;         /* Pointer for I/O request */
-UBYTE *keyMatrix = NULL;
-#define KEY_MATRIX_SIZE 16
+/* ViewPort 1 */
+#define WIDTH1   320 /* 320 pixels wide.                              */
+#define HEIGHT1  80 /* 150 lines high.                               */ 
+#define DEPTH1     5 /* 5 BitPlanes should be used, gives 32 colours. */
+#define COLOURS1  32 /* 2^5 = 32                                      */
 
-#define SCR_WIDTH           320
-#define SCR_HEIGHT          280
-#define SCR_DEPTH			5
+/* ViewPort 2 */
+#define WIDTH2   320 /* 640 pixels wide.                             */
+#define HEIGHT2   250-80 /* 45 lines high.                               */
+#define DEPTH2     3 /* 1 BitPlanes should be used, gives 2 colours. */
+#define COLOURS2   8 /* 2^1 = 2                                      */
 
-PLANEPTR theRaster;
-struct RastPort theRP;
-struct BitMap theBitMap;
-struct Screen *main_screen = NULL;
+
+struct IntuitionBase *IntuitionBase;
+struct GfxBase *GfxBase;
+
+
 struct View my_view;
+struct View *my_old_view;
 
-struct  BitMap *bitmap_logo = NULL;
 
-USHORT	dbl_buffer_index = 0;
-#define DBL_BUFFER_OFFSET (dbl_buffer_index * SCR_HEIGHT)
-#define DBL_BUFFER_SWAP dbl_buffer_index = !dbl_buffer_index;
-
-struct NewScreen theScreen16 =
+/* ViewPort 1 */
+struct ViewPort view_port1;
+struct RasInfo ras_info1;
+struct BitMap bit_map1;
+struct RastPort rast_port1;
+UWORD color_table1[] =
 {
-  0, 0, SCR_WIDTH, SCR_HEIGHT << 1, SCR_DEPTH, 0, 1, 0,
-  CUSTOMSCREEN | CUSTOMBITMAP | SCREENQUIET, NULL, NULL, NULL, &theBitMap
+	0x000, 0xFFF, 0xDDD, 0xBBB, 0x999, 0x777, 0x555, 0x333,
+	0xF00, 0xD00, 0xB00, 0x900, 0x700, 0x500, 0x300, 0x100,
+	0x0F0, 0x0D0, 0x0B0, 0x090, 0x070, 0x050, 0x030, 0x010,
+	0x00F, 0x00D, 0x00B, 0x009, 0x007, 0x005, 0x003, 0x001
 };
 
-struct Library *IntuitionBase = NULL;
-struct Library *GfxBase = NULL;
-extern struct Library *SysBase;
+
+/* ViewPort 2 */
+struct ViewPort view_port2;
+struct RasInfo ras_info2;
+struct BitMap bit_map2;
+struct RastPort rast_port2;
+UWORD color_table2[] = { 0x000, 0xFFF, 0xFFF, 0xFFF, 0xFFF, 0xFFF, 0xFFF, 0xFFF };
 
 void drawMandarineLogo(struct BitMap *dest_bitmap, USHORT offset_y, USHORT clip_y)
 {
-    /*
-        Logo
-    */
-	if (bitmap_logo == NULL)
-    	bitmap_logo = load_array_as_bitmap(mandarine_logoData, 8000 << 1, mandarine_logo.Width - 8, mandarine_logo.Height, mandarine_logo.Depth);
-
-    if (clip_y > mandarine_logo.Height)
-    	clip_y = mandarine_logo.Height;
-
-    if (clip_y > 0 && clip_y <= mandarine_logo.Height)
-    {
-    	if (clip_y >= mandarine_logo.Height)
-    		clip_y = mandarine_logo.Height - 1;
-
-	    BLIT_BITMAP_S(bitmap_logo, dest_bitmap, mandarine_logo.Width, clip_y, (SCR_WIDTH - mandarine_logo.Width) / 2, offset_y + mandarine_logo.Height - clip_y);
-	}
+	/*
+	Logo
+	*/
+	struct  BitMap *bitmap_logo;
+	// if (bitmap_logo == NULL)
+	bitmap_logo = load_array_as_bitmap(mandarine_logoData, 8000 << 1, mandarine_logo.Width - 8, mandarine_logo.Height, mandarine_logo.Depth);
+	BLIT_BITMAP_S(bitmap_logo, dest_bitmap, mandarine_logo.Width, mandarine_logo.Height, 0, 0);
 }
 
-void open_main_screen(void)
+void clean_up(STRPTR message);
+
+void main()
 {
-	int i, c;
+	UWORD *pointer;
+	int loop;
 
-	InitView(&my_view);
+	/* Open the Intuition library: */
+	IntuitionBase = (struct IntuitionBase *)
+	OpenLibrary( "intuition.library", 0 );
+	if( !IntuitionBase )
+		clean_up( "Could NOT open the Intuition library!" );
 
-	InitBitMap(&theBitMap, SCR_DEPTH, SCR_WIDTH, SCR_HEIGHT << 1);
+	/* Open the Graphics library: */
+	GfxBase = (struct GfxBase *)
+	OpenLibrary( "graphics.library", 0 );
+	if( !GfxBase )
+		clean_up( "Could NOT open the Graphics library!" );
 
-	for (i = 0; i < SCR_DEPTH; i++)
-		theBitMap.Planes[i] = AllocRaster(SCR_WIDTH, SCR_HEIGHT << 1);
+	/* Save the current View, so we can restore it later: */
+	my_old_view = GfxBase->ActiView;
 
-	InitRastPort(&theRP);
-	theRP.BitMap = &theBitMap;
-	SetRast(&theRP, 0);
+	/* 1. Prepare the View structure, and give it a pointer to */
+	/*    the first ViewPort:                                  */
+	InitView( &my_view );
+	my_view.ViewPort = &view_port1;
 
-	main_screen = OpenScreen(&theScreen16);
+	/* 2. Prepare the ViewPort structures, and set some important values: */
 
-	// my_view.ViewPort = &main_screen->ViewPort;
-	// MakeVPort( &my_view, &main_screen->ViewPort );
-	// MrgCop( &my_view );
-	// LoadView( &my_view );	
+	/* ViewPort 1 */
+	InitVPort( &view_port1 );
+	view_port1.DWidth = WIDTH1;      /* Set the width.                */
+	view_port1.DHeight = HEIGHT1;    /* Set the height.               */
+	view_port1.DxOffset = 0;         /* X position.                   */
+	view_port1.DyOffset = 0;         /* Y position.                   */
+	view_port1.RasInfo = &ras_info1; /* Give it a pointer to RasInfo. */
+	view_port1.Modes = NULL;         /* Low resolution.               */
+	view_port1.Next = &view_port2;   /* Pointer to next ViewPort.     */
 
-	// drawSomething(&theRP, 0);
-    for(c = 0; c < 31; c++)
-        SetRGB4(&main_screen->ViewPort, c, (mandarine_logoPaletteRGB4[c] & 0x0f00) >> 8, (mandarine_logoPaletteRGB4[c] & 0x00f0) >> 4, (mandarine_logoPaletteRGB4[c] & 0x000f));
+	/* ViewPort 2 */
+	InitVPort( &view_port2 );
+	view_port2.DWidth = WIDTH2;      /* Set the width.                */
+	view_port2.DHeight = HEIGHT2;    /* Set the height.               */
+	view_port2.DxOffset = 0;         /* X position.                   */
+	view_port2.DyOffset = HEIGHT1+5; /* Y position (5 lines under).   */
+	view_port2.RasInfo = &ras_info2; /* Give it a pointer to RasInfo. */
+	view_port2.Modes = NULL;        /* High resolution.              */
+	view_port2.Next = NULL;          /* Last ViewPort in the list.    */
 
-	// drawMandarineLogo(theRP.BitMap, 8, 0);
-}
+	/* 3. Get a colour map, link it to the ViewPort, and prepare it: */
 
-void close_main_screen(void)
-{
-	// int i;
-	// for (i = 0; i < SCR_DEPTH; i++)
-	// 	if (theBitMap.Planes[i]) FreeRaster(theBitMap.Planes[i], SCR_WIDTH, SCR_HEIGHT);
-	if (main_screen)
+	/* ViewPort 1 */
+	view_port1.ColorMap = (struct ColorMap *) GetColorMap( COLOURS1 );
+	if( view_port1.ColorMap == NULL )
+		clean_up( "Could NOT get a ColorMap!" );
+	/* Get a pointer to the colour map: */
+	pointer = (UWORD *) view_port1.ColorMap->ColorTable;
+	/* Set the colours: */
+	for( loop = 0; loop < COLOURS1; loop++ )
+		*pointer++ = mandarine_logoPaletteRGB4[ loop ];
+
+	/* ViewPort 2 */
+	view_port2.ColorMap = (struct ColorMap *) GetColorMap( COLOURS2 );
+	if( view_port2.ColorMap == NULL )
+		clean_up( "Could NOT get a ColorMap!" );
+	/* Get a pointer to the colour map: */
+	pointer = (UWORD *) view_port2.ColorMap->ColorTable;
+	/* Set the colours: */
+	for( loop = 0; loop < COLOURS2; loop++ )
+		*pointer++ = color_table2[ loop ];
+
+
+
+	/* 4. Prepare the BitMap: */
+
+	/* ViewPort 1 */
+	InitBitMap( &bit_map1, DEPTH1, WIDTH1, HEIGHT1 );
+	/* Allocate memory for the Raster: */ 
+	for( loop = 0; loop < DEPTH1; loop++ )
 	{
-		CloseScreen(main_screen);
-		main_screen = NULL;
+		bit_map1.Planes[ loop ] = (PLANEPTR) AllocRaster( WIDTH1, HEIGHT1 );
+		if( bit_map1.Planes[ loop ] == NULL )
+			clean_up( "Could NOT allocate enough memory for the raster!" );
+	/* Clear the display memory with help of the Blitter: */
+		BltClear( bit_map1.Planes[ loop ], RASSIZE( WIDTH1, HEIGHT1 ), 0 );
 	}
 
-	if (IntuitionBase)
+	/* ViewPort 2 */
+	InitBitMap( &bit_map2, DEPTH2, WIDTH2, HEIGHT2 );
+	/* Allocate memory for the Raster: */ 
+	for( loop = 0; loop < DEPTH2; loop++ )
 	{
-		CloseLibrary(IntuitionBase);
-		IntuitionBase = NULL;
-	}
-
-	if (GfxBase)
-	{
-		CloseLibrary(GfxBase);
-		GfxBase = NULL;
+		bit_map2.Planes[ loop ] = (PLANEPTR) AllocRaster( WIDTH2, HEIGHT2 );
+		if( bit_map2.Planes[ loop ] == NULL )
+			clean_up( "Could NOT allocate enough memory for the raster!" );
+		/* Clear the display memory with help of the Blitter: */
+		BltClear( bit_map2.Planes[ loop ], RASSIZE( WIDTH2, HEIGHT2 ), 0 );
 	}
 
 
-}
 
-void close_demo(void)
-{
-	close_main_screen();
+	/* 5. Prepare the RasInfo structure: */
 
-	// /*  Close the keyboard device */
-	// if (!(CheckIO((struct IORequest *)KeyIO)))
-	// 	AbortIO((struct IORequest *)KeyIO);   //  Ask device to abort request, if pending 
+	/* ViewPort 1 */
+	ras_info1.BitMap = &bit_map1; /* Pointer to the BitMap structure.  */
+	ras_info1.RxOffset = 0;       /* The top left corner of the Raster */
+	ras_info1.RyOffset = 0;       /* should be at the top left corner  */
+	              /* of the display.                   */
+	ras_info1.Next = NULL;        /* Single playfield - only one       */
+	              /* RasInfo structure is necessary.   */
 
-	// WaitIO((struct IORequest *)KeyIO);   /* Wait for abort, then clean up */
-	// CloseDevice((struct IORequest *)KeyIO);
-	// if (keyMatrix) FreeMem(keyMatrix,KEY_MATRIX_SIZE);
+	/* ViewPort 2 */
+	ras_info2.BitMap = &bit_map2; /* Pointer to the BitMap structure.  */
+	ras_info2.RxOffset = 0;       /* The top left corner of the Raster */
+	ras_info2.RyOffset = 0;       /* should be at the top left corner  */
+	              /* of the display.                   */
+	ras_info2.Next = NULL;        /* Single playfield - only one       */
+	              /* RasInfo structure is necessary.   */
 
-	exit(0);
-}
 
-int open_keyboard(void)
-{
-    if (KeyMP=CreatePort(NULL,NULL))
-      if (KeyIO=(struct IOStdReq *)CreateExtIO(KeyMP,sizeof(struct IOStdReq)))
-        if (OpenDevice( "keyboard.device",NULL,(struct IORequest *)KeyIO,NULL))
-        {
-          printf("keyboard.device did not open\n");
-          return(0);
-        }
-        else
-        if (!(keyMatrix=AllocMem(KEY_MATRIX_SIZE,MEMF_PUBLIC|MEMF_CLEAR)))
-        {
-          printf("Cannot allocate keyboard buffer\n");
-          return(0);
-        }
-}
 
-void sys_check_abort(void)
-{
-  KeyIO->io_Command=KBD_READMATRIX;
-  KeyIO->io_Data=(APTR)keyMatrix;
-  KeyIO->io_Length = SysBase->lib_Version >= 36 ? KEY_MATRIX_SIZE : 13;
-  DoIO((struct IORequest *)KeyIO);
+	/* 6. Create the display: */
+	MakeVPort( &my_view, &view_port1 ); /* Prepare ViewPort 1 */
+	MakeVPort( &my_view, &view_port2 ); /* Prepare ViewPort 2 */
+	MrgCop( &my_view );
 
-  if (keyMatrix[0x45 >> 3] & (0x20))
-    close_demo();
-}
 
-int main(void)
-{
-	int logo_y_clip = 0,
-			logo_y_clip_FP = 0;
 
-	IntuitionBase = OpenLibrary( "intuition.library", INTUITION_REV);
-	GfxBase = OpenLibrary("graphics.library", INTUITION_REV);	
+	/* 7. Prepare the RastPort, and give it a pointer to the BitMap. */
 
-	open_keyboard();
-	open_main_screen();
+	/* ViewPort 1 */
+	InitRastPort( &rast_port1 );
+	rast_port1.BitMap = &bit_map1;
 
-	printf("Mandarine!\n");
+	/* ViewPort 2 */
+	InitRastPort( &rast_port2 );
+	rast_port2.BitMap = &bit_map2;
 
-	while(TRUE)
+
+
+	/* 8. Show the new View: */
+	LoadView( &my_view );
+
+	drawMandarineLogo(&bit_map1, 0, mandarine_logo.Height);
+
+	/* Set the draw mode to JAM1. FgPen's colour will be used. */
+	SetDrMd( &rast_port1, JAM1 );
+	SetDrMd( &rast_port2, JAM1 );
+
+	/* Set FgPen's colour to 1 (white). */
+	SetAPen( &rast_port2, 1 );
+	/* Draw some pixels in the second ViewPort: */
+	for( loop = 0; loop < 500; loop++ )
+		WritePixel( &rast_port2, rand() % WIDTH2, rand() % HEIGHT2 );
+
+	/* Print some text into the second ViewPort: */
+	Move( &rast_port2, 0, 10 );
+	Text( &rast_port2, "This text is written on a single high resolution BitMap. The ViewPort above use ", 80 );
+	Move( &rast_port2, 0, 20 );
+	Text( &rast_port2, "a 32-colour low resolution BitMap.                                              ", 80 );
+
+	/* Draw 10000 pixels in seven different colours, randomly. */ 
+	for( loop = 0; loop < 1000; loop++ )
 	{
 		WaitTOF();
-		// MakeVPort( &my_view, &main_screen->ViewPort );
-		// MrgCop( &my_view );
-		// LoadView( &my_view );
-
-		main_screen->ViewPort.RasInfo->RxOffset = 0;
-		main_screen->ViewPort.RasInfo->RyOffset = DBL_BUFFER_OFFSET;
-		ScrollVPort(&(main_screen->ViewPort));
-
-		DBL_BUFFER_SWAP;
-
-		if (logo_y_clip <= mandarine_logo.Height << 4)
-		{
-			logo_y_clip_FP = logo_y_clip << 1;
-			logo_y_clip_FP *= logo_y_clip_FP;
-			logo_y_clip_FP = (logo_y_clip_FP >> 2) >> 4;
-
-			drawMandarineLogo(theRP.BitMap, 8 + DBL_BUFFER_OFFSET, (USHORT) logo_y_clip_FP);
-			logo_y_clip++;	
-		}
-
-		sys_check_abort();
+		/* Set FgPen's colour (1-31, 0 used for the the background). */
+		SetAPen( &rast_port1, rand() % (COLOURS1-1) + 1 );
+		/* Write a pixel somewere on the display: */
+		WritePixel( &rast_port1, rand() % WIDTH1, rand() % HEIGHT1 );
 	}
 
-	close_demo();
 
-	return(1);
+
+	/* 9. Restore the old View: */
+	LoadView( my_old_view );
+
+
+	/* Free all allocated resources and leave. */
+	clean_up( "THE END" );
+}
+
+
+/* Returns all allocated resources: */
+void clean_up(STRPTR message)
+{
+	int loop;
+
+	/* Free automatically allocated display structures: */
+	FreeVPortCopLists( &view_port1 );
+	FreeVPortCopLists( &view_port2 );
+	FreeCprList( my_view.LOFCprList );
+
+	/* Deallocate the display memory, BitPlane for BitPlane: */
+	for( loop = 0; loop < DEPTH1; loop++ )
+		if( bit_map1.Planes[ loop ] )
+			FreeRaster( bit_map1.Planes[ loop ], WIDTH1, HEIGHT1 );
+	for( loop = 0; loop < DEPTH2; loop++ )
+		if( bit_map2.Planes[ loop ] )
+			FreeRaster( bit_map2.Planes[ loop ], WIDTH2, HEIGHT2 );
+
+	/* Deallocate the ColorMap: */
+	if( view_port1.ColorMap ) FreeColorMap( view_port1.ColorMap );
+	if( view_port2.ColorMap ) FreeColorMap( view_port2.ColorMap );
+
+// /* Close the Graphics library: */
+// if( GfxBase ) CloseLibrary( GfxBase );
+
+//  Close the Intuition library: 
+// if( IntuitionBase ) CloseLibrary( IntuitionBase );
+
+	/* Print the message and leave: */
+	printf( "%s\n", message ); 
+	exit(0);
 }

e14a57c338021952e7aee415c878606ce74d5733
astrofra <astrofra@gmail.com>
2015-08-19 22:58:28 +0200

    - Conversion using the "plus" module.

--- pc_version/src/demo_simulation.py
+++ pc_version/src/demo_simulation.py
@@ -54,10 +54,12 @@ class DemoSimulation:
 						}
 
 		for texture_name in self.pictures:
-			texture_filename = os.path.join("res", texture_name + ".png")
+			texture_filename = os.path.join("..\\", "res", texture_name + ".png")
 			if os.path.exists(texture_filename):
 				self.pictures[texture_name] = gs.LoadPicture(texture_filename)
 				print("Found texture : ", texture_filename)
+			else:
+				print("Cannot find texture : ", texture_filename)
 
 		if self.pictures["checkerboard_strip"] is not None:
 			pixel_data = self.pictures["checkerboard_strip"].GetData()
--- pc_version/src/main.py
+++ pc_version/src/main.py
@@ -1,5 +1,12 @@
 import gs
-import screen_size
+import gs.plus.render as render
+import gs.plus.camera as camera
+import gs.plus.geometry as geometry
+import gs.plus.input as input
+import gs.plus.scene as scene
+import gs.plus.clock as clock
+import gs.plus.audio as audio
+import math
 from demo_simulation import DemoSimulation
 
 
@@ -8,59 +15,53 @@ def main():
 	demo_screen_width = 384
 	demo_screen_height = 280
 
-	# mount the system file driver
-	gs.GetFilesystem().Mount(gs.StdFileDriver("pkg.core"), "@core")
+	render.init(640, 400, "../pkg.core")
 	gs.MountFileDriver(gs.StdFileDriver())
 
-	# create the renderer and render system
-	egl = gs.EglRenderer()
-	egl.Open(1280, 720)
+	scn = scene.new_scene()
 
-	sys = gs.RenderSystem()
-	sys.Initialize(egl)
+	cam = scene.add_camera(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(0, 1, -10)))
+	scene.add_light(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(6, 4, -6)))
+	scene.add_cube(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(0, 0.5, 0)))
+	scene.add_plane(scn)
+	demo_screen = scene.add_plane(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(0, 2.0, 0)), 4 * 0.5, 3 * 0.5)
+	demo_screen.transform.SetRotation(gs.Vector3(math.pi * -0.5, 0.0, 0.0))
 
-	# create the font object
-	font = gs.RasterFont("@core/fonts/default.ttf", 48, 512)
-
-	# set default render states
-	egl.Set2DMatrices()
-	egl.EnableBlending(True)
-	egl.EnableDepthTest(False)
+	fps = camera.fps_controller(0, 2, -10)
 
 	# Init demo simulation
 	demo = DemoSimulation(demo_screen_width, demo_screen_height)
-
 	demo.load_textures()
-	demo_screen_tex = egl.NewTexture("demo_screen_texture")
+	# demo_screen_tex = egl.NewTexture("demo_screen_texture")
 
-	res = egl.CreateTexture(demo_screen_tex, demo.screen_pic)
+	# res = egl.CreateTexture(demo_screen_tex, demo.screen_pic)
 	# res = egl.CreateTexture(demo_screen_tex, demo_screen_pic.GetWidth(), demo_screen_pic.GetHeight())
-	print("CreateTexture() returned ", res)
 
-	demo_screen_vertices = [gs.Vector3(0.5 * 1280, 0.5 * 720, 0.5)]
+	# print("CreateTexture() returned ", res)
 
-	while egl.GetDefaultOutputWindow():
-		egl.Clear(gs.Color.Black)
+	audio.init()
+	audio.get_mixer().Stream("../res/music_loop.ogg")
+	plane_angle = 0.0
+
+	while not input.key_press(gs.InputDevice.KeyEscape):
+		dt_sec = clock.update()
 
 		# Demo simulation (re-creation)
+		demo.dt = dt_sec
 		demo.clear_screen()
 		demo.draw_pixel_art_logo()
 		demo.draw_checkerboard()
 		demo.draw_unlimited_bobs()
 		demo.render_demo_text()
+		# egl.BlitTexture(demo_screen_tex, demo.screen_pic)
 
-		egl.BlitTexture(demo_screen_tex, demo.screen_pic)
+		fps.update_and_apply_to_node(cam, dt_sec)
 
-		egl.SetBlendFunc(gs.GpuRenderer.BlendSrcAlpha, gs.GpuRenderer.BlendOneMinusSrcAlpha)
-		egl.EnableBlending(True)
-		sys.DrawSpriteAuto(1, demo_screen_vertices, demo_screen_tex, 0.5 * 720)
-
-		egl.DrawFrame()
-		egl.ShowFrame()
-		egl.UpdateOutputWindow()
+		scene.update_scene(scn, dt_sec)
+		render.text2d(5, 5, "Move around with QSZD, left mouse button to look around")
+		render.flip()
 
 	font = None
 
 if __name__ == "__main__":
     main()
-

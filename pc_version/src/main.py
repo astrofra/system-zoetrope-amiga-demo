0acdab385b4749a149eba2eef1af4b6d16babe23
astrofra <astrofra@gmail.com>
2015-09-02 22:23:40 +0200

    - Added a couple of geometries.

--- pc_version/src/main.py
+++ pc_version/src/main.py
@@ -16,17 +16,40 @@ def main():
 	demo_screen_width = 384
 	demo_screen_height = 280
 
-	render.init(1920, 1080, "pkg.core")
+	if os.path.exists("pkg.core"):
+		render.init(1280, 720, "pkg.core")
+	else:
+		print("Cannot find pkg.core/")
+		exit()
+
 	gs.MountFileDriver(gs.StdFileDriver())
 
 	scn = scene.new_scene()
 
-	cam = scene.add_camera(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(0, 1, -10)))
+	cam = scene.add_camera(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(0, 0, 0)))
 	scene.add_light(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(6, 4, -6)))
-	scene.add_cube(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(0, 0.5, 0)))
+	# scene.add_cube(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(0, 0.5, 0)))
 	scene.add_plane(scn)
 	# demo_screen = scene.add_plane(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(0, 2.0, 0)), 4 * 0.5, 3 * 0.5, os.path.join("res", "monitor", "monitor_screen.mat"))
 	# demo_screen.transform.SetRotation(gs.Vector3(math.pi * -0.5, 0.0, 0.0))
+
+	monitor_geo = render.load_geometry(os.path.join("res", "monitor", "monitor.geo"))
+	monitor_node = gs.Node("monitor")
+	monitor_transform = gs.Transform()
+	monitor_node.AddComponent(monitor_transform)
+	monitor_object = gs.Object()
+	monitor_object.SetGeometry(monitor_geo)
+	monitor_node.AddComponent(monitor_object)
+	scn.AddNode(monitor_node)
+
+	monitor_frame_geo = render.load_geometry(os.path.join("res", "monitor", "monitor_frame.geo"))
+	monitor_frame_node = gs.Node("monitor_frame")
+	monitor_frame_transform = gs.Transform()
+	monitor_frame_node.AddComponent(monitor_frame_transform)
+	monitor_frame_object = gs.Object()
+	monitor_frame_object.SetGeometry(monitor_frame_geo)
+	monitor_frame_node.AddComponent(monitor_frame_object)
+	scn.AddNode(monitor_frame_node)
 
 	demo_screen_geo = render.load_geometry(os.path.join("res", "monitor", "monitor_screen.geo"))
 	demo_screen_node = gs.Node("demo_screen")
@@ -39,17 +62,11 @@ def main():
 	demo_screen_mat = demo_screen_geo.GetMaterial(0)
 	demo_screen_tex = demo_screen_mat.GetTexture("diffuse_map")
 
-	fps = camera.fps_controller(0, 2, -10)
+	fps = camera.fps_controller(0, 0.5, -3.5)
 
 	# Init demo simulation
 	demo = DemoSimulation(demo_screen_width, demo_screen_height)
 	demo.load_textures()
-	# demo_screen_tex = egl.NewTexture("demo_screen_texture")
-
-	# res = egl.CreateTexture(demo_screen_tex, demo.screen_pic)
-	# res = egl.CreateTexture(demo_screen_tex, demo_screen_pic.GetWidth(), demo_screen_pic.GetHeight())
-
-	# print("CreateTexture() returned ", res)
 
 	audio.init()
 	demo_audio_stream = audio.get_mixer().Stream(os.path.join("res", "music_loop.ogg"))

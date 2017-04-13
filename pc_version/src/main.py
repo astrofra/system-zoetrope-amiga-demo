223a89b70773e452ac06903aeb461ebd27eb4ba2
astrofra <astrofra@gmail.com>
2017-04-13 20:55:55 +0200

    - Updated the PC version, improved the fullscreen VS windowed mode.

--- pc_version/src/main.py
+++ pc_version/src/main.py
@@ -5,11 +5,16 @@ from demo_simulation import DemoSimulation
 
 def main():
 	demo = None
+	pc_screen_windowed = True
 	pc_screen_width = 1280
 	pc_screen_height = 720
+
 	demo_screen_width = 384
 	demo_screen_height = 280
 	amiga_screen_ratio = demo_screen_height / demo_screen_width
+
+	if pc_screen_windowed:
+		pc_screen_width = int(pc_screen_height * (demo_screen_width / demo_screen_height))
 
 	# mount the system file driver
 	# gs.GetFilesystem().Mount(gs.StdFileDriver("pkg.core"), "@core")
@@ -18,7 +23,7 @@ def main():
 
 	# create the renderer and render system
 	plus = gs.GetPlus()
-	plus.RenderInit(1280, 720)
+	plus.RenderInit(pc_screen_width, pc_screen_height)
 
 	egl = plus.GetRenderer()
 
@@ -37,7 +42,8 @@ def main():
 	# play music
 	al = gs.MixerAsync(gs.ALMixer())
 	al.Open()
-	future_channel = al.Stream("res/music_loop.ogg")
+	channel_state = gs.MixerChannelState(0, 1, gs.MixerRepeat)
+	future_channel = al.Stream("res/music_loop.ogg", channel_state)
 
 	while not plus.KeyPress(gs.InputDevice.KeyEscape):
 		dt = plus.UpdateClock()
@@ -53,10 +59,15 @@ def main():
 
 		egl.BlitTexture(demo_screen_tex, demo.screen_pic)
 
-		_x_offset = (pc_screen_width - pc_screen_width * amiga_screen_ratio) * 0.5
-		plus.Quad2D(_x_offset, 0, _x_offset, pc_screen_height, _x_offset + (pc_screen_width * amiga_screen_ratio),
-					pc_screen_height, _x_offset + (pc_screen_width * amiga_screen_ratio), 0,
-					gs.Color.White, gs.Color.White, gs.Color.White, gs.Color.White, demo_screen_tex)
+		if pc_screen_windowed:
+			plus.Quad2D(0, 0, 0, pc_screen_height, pc_screen_width, pc_screen_height,
+						pc_screen_width, 0, gs.Color.White, gs.Color.White, gs.Color.White, gs.Color.White,
+						demo_screen_tex)
+		else:
+			_x_offset = (pc_screen_width - pc_screen_width * amiga_screen_ratio) * 0.5
+			plus.Quad2D(_x_offset, 0, _x_offset, pc_screen_height, _x_offset + (pc_screen_width * amiga_screen_ratio),
+						pc_screen_height, _x_offset + (pc_screen_width * amiga_screen_ratio), 0,
+						gs.Color.White, gs.Color.White, gs.Color.White, gs.Color.White, demo_screen_tex)
 
 		plus.Flip()
 
